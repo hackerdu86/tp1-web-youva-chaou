72d6b09b05dfe6b280eb47adfa293ee23dedec9c
Youva Chaou <2125762@cmontmorency.qc.ca>
2023-03-02 14:56:24 -0500

    Implémentation de la barre de navigation

--- tp1-youva-chaou/src/components/UpperNavBar.js
+++ tp1-youva-chaou/src/components/UpperNavBar.js
@@ -1,19 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "../styles/navbar.css"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/navbar.css";
 
 function UpperNavBar() {
-    return (
-        <nav className="navbar">
-          <div className="navbar-left">
-          <Link to="/menu-principal">Menu Principal</Link>
-          </div>
-          <div className="navbar-right">
-            <Link to="/profs">Profs</Link>
-            <Link to="/cours">Cours</Link>
-          </div>
-        </nav>
-      );
+  function checkDeviceMobile() {
+    return window.innerWidth < 768 ? true : false;
+  }
+
+  const [isDeviceMobile, setIsDeviceMobile] = useState(checkDeviceMobile());
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleMenuClick() {
+    setIsOpen(!isOpen);
+  }
+
+  function handleOutsideClick() {
+    if (isOpen) {
+      setIsOpen(false);
+    }
+  }
+
+  window.addEventListener("resize", (event) => {
+    console.log(checkDeviceMobile());
+    setIsDeviceMobile(checkDeviceMobile());
+  });
+
+  return !isDeviceMobile ? (
+    <nav className="navbar-top">
+      <div className="lol">
+        <Link to="/menu-principal">Menu Principal</Link>
+      </div>
+      <div className="navbar-right">
+        <Link to="/profs">Profs</Link>
+        <Link to="/cours">Cours</Link>
+      </div>
+    </nav>
+  ) : (
+    <>
+      <Link to="/menu-principal">
+        <button className="menu-button" onClick={handleMenuClick}>
+          Menu Principal
+        </button>
+      </Link>
+
+      <nav
+        className={isOpen ? "nav-bar open" : "nav-bar"}
+        onClick={handleOutsideClick}
+      >
+        <button className="nav-button top-button">Menu principal</button>
+        <div className="nav-buttons-bottom">
+          <Link to="/profs">
+            {" "}
+            <button className="nav-button">Profs</button>
+          </Link>
+          <Link to="/cours">
+            {" "}
+            <button className="nav-button">Cours</button>
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default UpperNavBar;
